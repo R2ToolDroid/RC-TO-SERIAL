3eba9e769afa06b074e0d9b906b52054e9ae1ffd
Doc Snyder <44336447+R2ToolDroid@users.noreply.github.com>
2021-12-21 13:11:46 +0100

    Update RC-TO-SERIAL.ino
    Test with 8 Commands

--- RC-TO-SERIAL.ino
+++ RC-TO-SERIAL.ino
@@ -4,19 +4,24 @@
 //  RC INPUT CHANNEL FROM variable PIN  
 
 //Indicator LED to check if RC is valid
-const int ledPin =  LED_BUILTIN;        // the number of the LED pin
+const int ledPin =  17;        // the number of the LED pin
 int ledState = LOW;                     // ledState used to set the LED
 unsigned long previousMillis = 0;        // will store last time LED was updated
-const long interval = 1000;               // interval at which to blink (milliseconds)
+const long interval = 400;               // interval at which to blink (milliseconds)
 
-#define ACTIVEPIN 2  // Amount of Active Pins
+//#define DEBUG //
+//#define ACTIVEPIN 2  // Amount of Active Pins
+//int activePin[ACTIVEPIN] = {2,3};  //Array with RC Input Pins 
 
-int activePin[ACTIVEPIN] = {2,4};  //Array with RC Input Pins 
+#define ACTIVEPIN 8
+int activePin[ACTIVEPIN] = {2,3,4,5,6,7,8,9};  //Array with RC Input Pins 
+
 
 //Set Comand for each PIN (RC Channel)
-
-String outHigh[ACTIVEPIN] = {"zweiH","vierH"};
-String outLow [ACTIVEPIN] = {"zweiL","vierL"};
+//  Commands               2|Wake UP 3|Cantina 4|Smirk 5|OP All  6| Arms OP  7|Arm Test 8| center 9| Mode                
+String outHigh[8] = {":SE14",":SE07",":SE03",":OP00","#OPUA/r#OPLA","#OPUA","center","MOD" };
+//  Commands                2|Reset 3| MANAMA 4|Wave 5|Flutter 6| Arms CL  7|Arm Test  8| tool1 9| next Sound
+String outLow [8] = {"CBD1","manama",":SE04","#OF00","#CLUA/r#CLLA","#CLUA","tool1","$2"};
 
 void getCMD(int PIN){
      
@@ -25,21 +30,23 @@ void getCMD(int PIN){
     for (int i=0 ; i <= PIN-1; i++){
       
         int value = pulseIn(activePin[i],HIGH);
-       // Serial.print("index ");
-        //Serial.print(i);
-        //Serial.print(" PIN ");
-        //Serial.print(activePin[i]);
-        //Serial.print(" val: ");
-        //Serial.println(value);
+        #ifdef DEBUG
+          Serial.print("index ");
+          Serial.print(i);
+          Serial.print(" PIN ");
+          Serial.print(activePin[i]);
+          Serial.print(" val: ");
+          Serial.println(value);
+        #endif
 
-        if (value <= 1200){
-          Serial.println(outLow[i]);
-          Serial.print('\r');
+        if (value <= 1100){
+          Serial.print(outLow[i]);
+          Serial.println('\r');
           delay(2000);
         }
-        if (value >= 1500){
-          Serial.println(outHigh[i]);
-          Serial.print('\r');
+        if (value >= 1600){
+          Serial.print(outHigh[i]);
+          Serial.println('\r');
           delay(2000);
         }
    
