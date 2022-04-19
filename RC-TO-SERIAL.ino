3d6f85fa422918deb1c76eeceb59ba71d959dc90
Doc Snyder <44336447+R2ToolDroid@users.noreply.github.com>
2022-04-19 10:27:48 +0200

    Change Reset to SE10

--- RC-TO-SERIAL.ino
+++ RC-TO-SERIAL.ino
@@ -19,13 +19,13 @@ int activePin[ACTIVEPIN] = {2,3,4,5,6,7,8,9};  //Array with RC Input Pins
 /* Channel --- Test for Pro PSI @0T7
 UP      Sound +    CantinaSE07    LeiaSE08    SmirkSE03       Open All      Arms OPA     Flutter        Sound Next$2
     ||-----1-----||-----2-----||-----3-----||-----4-----|    |-----5-----||-----6-----||-----7-----||-----8-----||  
-Down    Sound -     Manamana     FaintSE06    Wave2SE04       AwakeSE14     Spray     ResetSE00       Mode
+Down    Sound -     Manamana     FaintSE06    Wave2SE04       AwakeSE14     Spray     ResetSE10       Mode
 
 */
 //Set Comand for each PIN (RC Channel)
               
 String outLow[8] = {  "$+",":SE07",  ":SE08", ":SE03", ":OP00", "OPA", "#OF00",  "$2" };
-String outHigh [8] = {"$-","manama", ":SE06", ":SE04", ":SE14", "#SPRAY", ":SE00", "MOD"};
+String outHigh [8] = {"$-","manama", ":SE06", ":SE04", ":SE14", "#SPRAY", ":SE10", "MOD"};
 
 void getCMD(int PIN){
      
